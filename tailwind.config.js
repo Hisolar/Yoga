53f5b9ae3555959554caa7247fffd5b425d04da4
Hisolar <babatundeisola@gmail.com>
2023-05-18 04:39:20 -0700

    All responsiveness done

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,24 +16,24 @@ module.exports = {
         "screen-80": "80vh",
         "screen-90": "90vh",
       }),
-      screen: {
-        sm: "640px",
-        // => @media (min-width: 640px) { ... }
+      // screen: {
+      //   sm: "640px",
+      //   // => @media (min-width: 640px) { ... }
 
-        md: "768px",
-        // => @media (min-width: 768px) { ... }
+      //   md: "768px",
+      //   // => @media (min-width: 768px) { ... }
 
-        mac: "1290px",
+      //   mac: "1290px",
 
-        lg: "1094px",
-        // => @media (min-width: 1024px) { ... }
+      //   lg: "1094px",
+      //   // => @media (min-width: 1024px) { ... }
 
-        xl: "1280px",
-        // => @media (min-width: 1280px) { ... }
+      //   xl: "1280px",
+      //   // => @media (min-width: 1280px) { ... }
 
-        "2xl": "1536px",
-        // => @media (min-width: 1536px) { ... }
-      },
+      //   "2xl": "1536px",
+      //   // => @media (min-width: 1536px) { ... }
+      // },
 
       colors: {
         accent1: {
